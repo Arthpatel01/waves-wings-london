5c9ae549154c530b0ba01a3c2a30eadfda5625fb
Arthpatel01 <arthpatel.ce@gmail.com>
2026-05-23 23:00:05 +0530

    adding robots.txt and sitemaps.py for sitemaps and google bot.

--- AppHome/urls.py
+++ AppHome/urls.py
@@ -1,8 +1,19 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
+from django.views.generic import TemplateView
+
+from AppHome.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 from AppHome.views import IndexView, PrivacyPolicyView, TermsConditionsView
+
 urlpatterns = [
     path("", IndexView.as_view(), name="home"),
     path("privacy-policy/", PrivacyPolicyView.as_view(), name="privacy"),
     path("terms-conditions/", TermsConditionsView.as_view(), name="terms"),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
